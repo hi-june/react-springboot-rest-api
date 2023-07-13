a030d705aab8fc26d7c82a2769868d8553c071d7
hi-june <wnsgur735@gmail.com>
2023-07-14 01:02:42 +0900

    feat: product repository 정의

---
+++ caffe/src/main/java/com/programmers/caffe/repository/ProductRepository.java
@@ -0,0 +1,24 @@
+package com.programmers.caffe.repository;
+
+import com.programmers.caffe.model.Category;
+import com.programmers.caffe.model.Product;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ProductRepository {
+    List<Product> findAll();
+
+    Product insert(Product product);
+
+    Product update(Product product);
+
+    Optional<Product> findById(UUID productId);
+
+    Optional<Product> findByName(String productName);
+
+    List<Product> findByCategory(Category category);
+
+    void deleteAll();
+}
